9165f426098c5cfad86085fdaf0ae712694bca82
Axel JACQUOT <axel.jacquot@outlook.com>
2019-05-14 12:45:05 +0200

    Update recup

--- Scanner_3D/recup_coord.py
+++ Scanner_3D/recup_coord.py
@@ -1,11 +1,19 @@
 import os
 import numpy as np
+import math
 
 mot1 = ''
 mot2 = ''
 mot3 = ''
 
 
+angle_degree = 25
+
+angle_radian = math.radians(angle_degree)
+
+rotY = np.array([[math.cos(angle_radian),0,math.sin(angle_radian)],
+                  [0,1,0],
+                  [-(math.sin(angle_radian)),0,math.cos(angle_radian)]])
 
 mon_fichier = open("test0.pcd", "r")
 contenu = mon_fichier.readlines()
@@ -16,6 +24,7 @@ for i in range(11):
 i = i + 1
 n = 0
 while i < taille:
+    contenu_line_tmp = [0,0,0]
     contenu_line = [0,0,0]
     test_tmp = ''
     test = contenu[i]
@@ -23,9 +32,15 @@ while i < taille:
     for j in range(taille_contenu):
         if test[j] != '\n':
             test_tmp = test_tmp + test[j]
-    contenu_line = test_tmp.split(" ")
+    contenu_line_tmp = test_tmp.split(" ")
+    
+    points = np.array([[float(contenu_line_tmp[0])],
+                       [float(contenu_line_tmp[1])],
+                       [float(contenu_line_tmp[2])]])
+    
+    pointsf = np.dot(rotY, points)
                 
-    print(contenu_line)
+    print(pointsf)
    
     i = i + 1
 
